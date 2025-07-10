b0d38eabebad0e1ac806438b556d6922f2bce3e1
Lisa Louison <lisa.louison@gmail.com>
2025-07-09 20:24:46 -0500

    have a working fetch

--- README.md
+++ README.md
@@ -8,3 +8,7 @@
 - https://www.youtube.com/watch?v=B8Ihv3xsWYs
 - https://developers.google.com/youtube/v3/getting-started
 - https://developers.google.com/youtube/v3/docs/?apix=true
+
+# Goal
+
+Build a chrome extension that searches an existing youtube playlist and returns a list of video links as results.
--- script.js
+++ script.js
@@ -1,25 +1,19 @@
 async function fetchData() {
-  //   const options = {
-  //     method: 'GET',
-  //     // headers: {
-  //     //   'X-RapidAPI-Key': '[INSERT API KEY]',
-  //     //   'X-RapidAPI-Host': 'concerts-artists-events-tracker.p.rapidapi.com',
-  //     // },
-  //   };
-
   fetch('https://jsonplaceholder.typicode.com/posts')
     .then((response) => {
       return response.json();
     })
     .then((data) => {
       console.log(data);
+      // iterate through data
+      data.forEach((e) => {
+        const resultItem = document.createElement('li');
+        document.getElementById('resultsContainer')?.appendChild(resultItem);
+        resultItem.classList.add('resultItem');
+        resultItem.innerText = `video: ${e.title}`;
+      });
+      return data;
     });
-  //   const res = fetch('https://jsonplaceholder.typicode.com/posts', options);
-  //   const record = await res.json();
-
-  //   document.getElementById('concerts').innerHTML = record.data
-  //     .map((item) => `<li>${item.name}</li>`)
-  //     .join('');
 }
 fetchData();
 
