4fee32208aed90d74287c50f767d9df57dbb80b5
Lisa Louison <lisa.louison@gmail.com>
2025-07-09 19:24:13 -0500

    adding resources

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# hackathon-1-
+# hackathon-1
+
+# Resources
+
+- https://developer.chrome.com/docs/extensions/get-started/tutorial/hello-world
+- https://developer.chrome.com/docs/extensions/get-started/tutorial/debug
+- https://jsonplaceholder.typicode.com/guide/
+- https://www.youtube.com/watch?v=B8Ihv3xsWYs
+- https://developers.google.com/youtube/v3/getting-started
+- https://developers.google.com/youtube/v3/docs/?apix=true
--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//do something.
+async function fetchData() {
+  //   const options = {
+  //     method: 'GET',
+  //     // headers: {
+  //     //   'X-RapidAPI-Key': '[INSERT API KEY]',
+  //     //   'X-RapidAPI-Host': 'concerts-artists-events-tracker.p.rapidapi.com',
+  //     // },
+  //   };
+
+  fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+  //   const res = fetch('https://jsonplaceholder.typicode.com/posts', options);
+  //   const record = await res.json();
+
+  //   document.getElementById('concerts').innerHTML = record.data
+  //     .map((item) => `<li>${item.name}</li>`)
+  //     .join('');
+}
+fetchData();
+
+console.log('in extension');
